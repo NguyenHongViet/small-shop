d40d42146d1892ba9ffe0388e1f8de34d69c2984
Viet <banviet2222@gmail.com>
2020-05-01 13:08:23 +0700

    Add skeleton pages

--- app/controllers/admin/page_controller.rb
+++ app/controllers/admin/page_controller.rb
@@ -1,19 +1,29 @@
 class Admin::PageController < ApplicationController
   layout 'admin'
 
-  def dashboard
+  def dashboard; end
 
-  end
+  def account_list; end
 
-  def account_list
-    
-  end
+  def account_info; end
 
-  def account_info
-    
-  end
+  def account_form; end
 
-  def account_form
-    
-  end
+  def user_list; end
+
+  def user_info; end
+
+  def user_form; end
+
+  def product_list; end
+
+  def product_info; end
+
+  def product_form; end
+
+  def order_list; end
+
+  def order_info; end
+
+  def order_form; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,14 @@ Rails.application.routes.draw do
     get 'account/list', to: 'page#account_list'
     get 'account/form', to: 'page#account_form'
     get 'account/info', to: 'page#account_info'
+    get 'user/list', to: 'page#user_list'
+    get 'user/form', to: 'page#user_form'
+    get 'user/info', to: 'page#user_info'
+    get 'product/list', to: 'page#product_list'
+    get 'product/form', to: 'page#product_form'
+    get 'product/info', to: 'page#product_info'
+    get 'order/list', to: 'page#order_list'
+    get 'order/form', to: 'page#order_form'
+    get 'order/info', to: 'page#order_info'
   end
 end
