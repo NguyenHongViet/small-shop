e9c253e3ed4d35db74f5774e16156fdb72015838
Viet <banviet2222@gmail.com>
2020-05-03 11:04:17 +0700

    Login form

---
+++ app/controllers/admin/login_controller.rb
@@ -0,0 +1,7 @@
+class Admin::LoginController < ApplicationController
+  layout 'login'
+
+  def login; end
+
+  def password; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'admin/page#account_list'
+  root to: 'admin/login#login'
 
   namespace :admin do
+    get 'login', to: 'login#login'
+    get 'password', to: 'login#password'
+
     get 'account/list', to: 'page#account_list'
     get 'account/form', to: 'page#account_form'
     get 'account/info', to: 'page#account_info'
